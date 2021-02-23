45a945f9876ad5859691646adf765d327d761e2a
Bobcat <bobcat4848@gmail.com>
2021-02-23 17:15:56 -0600

    mic stuff :microphone:

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+var getUserMedia = require('get-user-media-promise');
+var MicrophoneStream = require('microphone-stream');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	var micStream = new MicrophoneStream();
+
 	console.log('Congratulations, your extension "voice-sc" is now active!');
+	let turnOnStream = vscode.commands.registerCommand('voice-sc.turnOnStream', () => {
+		vscode.window.showInformationMessage('Microphone stream is on...');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('voice-sc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from voice-sc!');
+		getUserMedia({ video: false, audio: true })
+			.then(function(stream: any) {
+				micStream.setStream(stream);
+			}).catch(function(error: any) {
+				console.log(error);
+			});
+
+		// get Buffers (Essentially a Uint8Array DataView of the same Float32 values)
+		micStream.on('data', function(chunk: any) {
+    // Optionally convert the Buffer back into a Float32Array
+    // (This actually just creates a new DataView - the underlying audio data is not copied or modified.)
+    var raw = MicrophoneStream.toRaw(chunk);
+    //...
+
+    // note: if you set options.objectMode=true, the `data` event will output AudioBuffers instead of Buffers
+   });
+
+  // or pipe it to another stream
+  micStream.pipe(/*...*/);
+
+  // Access the internal audioInput for connecting to another nodes
+  micStream.audioInput.connect(/*...*/);
 	});
 
-	context.subscriptions.push(disposable);
+	let turnOffStream = vscode.commands.registerCommand('voice-sc.turnOffStream', () => {
+		vscode.window.showInformationMessage('Microphone stream is off...');
+		micStream.pauseRecording();
+	});
+
+	// Push Commands
+	context.subscriptions.push(turnOnStream);
+	context.subscriptions.push(turnOffStream);
 }
 
 // this method is called when your extension is deactivated
